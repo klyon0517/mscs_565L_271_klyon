fc375e5fc0bf82a820d020db9b04cb7b901709b2
klyon0517@gmail.com <klyon0517@gmail.com>
2023-09-20 00:41:26 -0400

    Figured out how to transfer properties between projects using project sheets (.props). Also created game #2 Pong (and used a property sheet).

---
+++ Pong/Pong.cpp
@@ -0,0 +1,16 @@
+/*  Timber
+
+    * Course: Game Design & Programming I
+    * Type: Independent Study
+    * SEC: 565L 271
+    * CRN: 13015
+    * Instructor: Matthew Johnson
+    * Software: Game #2 (Beginning C++ Game Programming, Horton, J.)
+    * Filename: Pong.cpp
+    * Author: Kerry Lyon
+    * Created: September 19, 2023
+    * Due: October 5, 2023
+
+    * Main function for the Pong game.
+
+*/
