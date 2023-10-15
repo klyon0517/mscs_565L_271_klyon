d3e2f37fd7839d0f4be64a0f136e5f286eec1ca9
klyon0517@gmail.com <klyon0517@gmail.com>
2023-10-15 14:39:58 -0400

    Added more info to the vertices array. This time for color information. Adjusted the stride of the shader to accout for position and color attributes.

--- OpenGL/OpenGL.cpp
+++ OpenGL/OpenGL.cpp
@@ -41,18 +41,21 @@ const unsigned int SCR_HEIGHT = 600;
 // Vertex Shader
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"    gl_Position = vec4(aPos, 1.0);\n"
+"    ourColor = aColor;\n"
 "}\0";
 
 // Fragment Shader
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
-"uniform vec4 ourColor;\n"
+"in vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"    FragColor = ourColor;\n"
+"    FragColor = vec4(ourColor, 1.0);\n"
 "}\n\0";
 
 // Fragment Shader
@@ -169,11 +172,19 @@ int main() {
     glDeleteShader(fragmentShader);
 
     // Vertex Data
-    float vertices[] = {
+    /* float vertices[] = {
          0.5f,  0.5f, 0.0f, // top right
          0.5f, -0.5f, 0.0f, // bottom right
         -0.5f, -0.5f, 0.0f, // bottom left
         -0.5f,  0.5f, 0.0f  // top left
+    }; */
+    
+    float vertices[] = {
+        // positions         // colors
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // bottom right
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // bottom left
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // top
+
     };
 
     /* float vertices[] = {
@@ -209,7 +220,7 @@ int main() {
     // glGenVertexArrays(2, VAO);
     glGenBuffers(1, &VBO);
     // glGenBuffers(2, VBO);
-    glGenBuffers(1, &EBO);
+    // glGenBuffers(1, &EBO);
 
     // 1. bind Vertex Array Object
     glBindVertexArray(VAO);
@@ -219,12 +230,16 @@ int main() {
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     // 3. copy our index array in a element buffer for OpenGL to use
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // 4. then set the vertex attributes pointers
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    // Position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    // Color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
 
     // Second Triangle
     /* glBindVertexArray(VAO[1]);
--- OpenGL/RenderWindow.cpp
+++ OpenGL/RenderWindow.cpp
@@ -14,24 +14,24 @@ void renderWindow(
     glUseProgram(shaderProgram);
 
     // update the uniform color
-    float timeValue = glfwGetTime();
+    /* float timeValue = glfwGetTime();
     float greenValue = sin(timeValue) / 2.0f + 0.5f;
     int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-    glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+    glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f); */
 
     // render the triangle
-    glBindVertexArray(VAO);
-    // glDrawArrays(GL_TRIANGLES, 0, 3);
+    // glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
     // glBindVertexArray(VAO[1]);
     // glDrawArrays(GL_TRIANGLES, 0, 3);
     // glDrawArrays(GL_TRIANGLES, 0, 6);
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-    glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+    // glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 
-    glUseProgram(shaderProgram2);
-    glBindVertexArray(VAO);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)(3 * sizeof(VAO)));
+    // glUseProgram(shaderProgram2);
+    // glBindVertexArray(VAO);
+    // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)(3 * sizeof(VAO)));
     
     glfwSwapBuffers(window);
 
