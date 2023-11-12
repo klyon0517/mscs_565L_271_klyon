88ce4a2dcdd1431083f0a976ddc38ebef7a4b920
klyon0517@gmail.com <klyon0517@gmail.com>
2023-11-12 17:40:01 -0500

    Initial project creation / setup of game #5 - Space Invaders ++

---
+++ Space Invaders ++/Space Invaders ++.cpp
@@ -0,0 +1,23 @@
+/*  Space Invaders ++
+
+    * Course: Game Design & Programming I
+    * Type: Independent Study
+    * SEC: 565L 271
+    * CRN: 13015
+    * Instructor: Matthew Johnson
+    * Software: Game #5 (Beginning C++ Game Programming, Horton, J.)
+    * Filename: Space Invaders ++.cpp
+    * Author: Kerry Lyon
+    * Created: November 12, 2023
+    * Due: N/A
+
+    * Main function for the Space Invaders ++ game.
+
+*/
+
+int main()
+{
+    
+    // Quit in the usual way
+    return 0;
+}
