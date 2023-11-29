1bc6887dcae961fde94bf8e3bc9b09b451db78f8
klyon0517@gmail.com <klyon0517@gmail.com>
2023-11-28 21:15:04 -0500

    Finished coding the GameObject. What a beast!

---
+++ Space Invaders ++/GameObject.h
@@ -0,0 +1,50 @@
+#pragma once
+#include "Component.h"
+#include "GameObjectSharer.h"
+#include "GraphicsComponent.h"
+#include <SFML/Graphics.hpp>
+#include <string>
+#include "UpdateComponent.h"
+#include <vector>
+
+class GameObject
+{
+private:
+    std::vector<std::shared_ptr<Component>> m_Components;
+    std::string m_Tag;
+    bool m_Active = false;
+    int m_NumberUpdateComponents = 0;
+    bool m_HasUpdateComponent = false;
+    int m_FirstUpdateComponentLocation = -1;
+    int m_GraphicsComponentLocation = -1;
+    bool m_HasGraphicsComponent = false;
+    int m_TransformComponentLocation = -1;
+    int m_NumberRectColliderComponents = 0;
+    int m_FirstRectColliderComponentLocation = -1;
+    bool m_HasCollider = false;
+
+public:
+    void update(float fps);
+    void draw(sf::RenderWindow& window);
+    void addComponent(std::shared_ptr<Component> component);
+    void setActive();
+    void setInactive();
+    bool isActive();
+    void setTag(sf::String tag);
+    std::string getTag();
+    void start(GameObjectSharer* gos);
+
+    // slow only use in init and start
+    std::shared_ptr<Component> getComponentByTypeAndSpecificType(
+        std::string type,
+        std::string specificType
+    );
+    
+    sf::FloatRect& getEncompassingRectCollider();
+    bool hasCollider();
+    bool hasUpdateComponent();
+    std::string getEncompassingRectColliderTag();
+    std::shared_ptr<GraphicsComponent> getGraphicsComponent();
+    std::shared_ptr<TransformComponent> getTransformComponent();
+    std::shared_ptr<UpdateComponent> getFirstUpdateComponent();
+};
